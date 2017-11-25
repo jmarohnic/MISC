f1af6567f3bda18412ba8324cb1f93f6614df00a
Julian Marohnic <julian@Julians-iMac.local>
2017-11-24 23:18:04 -0500

    hydro changes

--- HW5/hydro.py
+++ HW5/hydro.py
@@ -1,7 +1,21 @@
 import numpy as np
 
-#419 1436
-#1439 416
+#Define system variables.
+v = 1
+gamma = 1.4
 
+def p(state):
+    return p = (gamma - 1) * state[0] * state[2]
 
+#Define F(u).
+def F(state):
+    new_state = []
+    new_state[1] = state[1]
+    new_state[2] = state[1] * v + p(state)
+    new_state[3] = (state[2] + p(state)) * v
+    return new_state
 
+#Define Lax scheme.
+def lax(state):
+    new_state = []
+    new_state = 0.5 * (state
